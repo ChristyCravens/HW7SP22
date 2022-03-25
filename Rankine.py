d75225cefcecc1ccadbf4b3493b89ac839eb252c
Christy C <102319832+ChristyCravens@users.noreply.github.com>
2022-03-25 17:10:01 -0500

    Add files via upload

---
+++ Rankine.py
@@ -0,0 +1,113 @@
+import numpy as np
+
+from Steam import steam
+import matplotlib.pyplot as plt
+
+class rankine():
+    def __init__(self, p_low=8, p_high=8000, eff_turbine=0.95, t_high=None, name='Rankine Cycle'):
+        '''
+        Constructor for rankine power cycle.  If t_high is not specified, the State 1
+        is assigned x=1 (saturated steam @ p_high).  Otherwise, use t_high to find State 1.
+        :param p_low: the low pressure isobar for the cycle in kPa
+        :param p_high: the high pressure isobar for the cycle in kPa
+        :param t_high: optional temperature for State1 (turbine inlet) in degrees C
+        :param name: a convenient name
+        '''
+        self.p_low=p_low
+        self.p_high=p_high
+        self.t_high=t_high
+        self.name=name
+        self.efficiency=None
+        self.turbine_work=0
+        self.pump_work=0
+        self.heat_added=0
+        self.state1=None
+        self.state2=None
+        self.state3=None
+        self.state4=None
+        self.eff_turbine=eff_turbine # Rankine class modified to include a value for isentropic turbine efficiency
+
+    def calc_efficiency(self):
+        #calculate the 4 states
+        #state 1: turbine inlet (p_high, t_high) superheated or saturated vapor
+        if(self.t_high==None):
+            self.state1 = steam(self.p_high, x=1.0, name='Turbine Inlet') # instantiate a steam object with conditions of state 1 as saturated steam, named 'Turbine Inlet'
+        else:
+            self.state1= steam(self.p_high, T=self.t_high, name='Turbine Inlet') # instantiate a steam object with conditions of state 1 at t_high, named 'Turbine Inlet'
+        #state 2: turbine exit (p_low, s=s_turbine inlet) two-phase
+        self.state2= steam(self.p_low, s=self.state1.s, name="Turbine Exit") # instantiate a steam object with conditions of state 2, named 'Turbine Exit'
+        #state 3: pump inlet (p_low, x=0) saturated liquid
+        self.state3= steam(self.p_low, x=0, name='Pump Inlet') # instantiate a steam object with conditions of state 3 as saturated liquid, named 'Pump Inlet'
+        #state 4: pump exit (p_high,s=s_pump_inlet) typically sub-cooled, but estimate as saturated liquid
+        self.state4=steam(self.p_high, s=self.state3.s, name='Pump Exit')
+        self.state4.h=self.state3.h+self.state3.v*(self.p_high-self.p_low)
+
+        self.turbine_work= (self.state1.h - self.state2.h)*self.eff_turbine # calculate turbine work #multiplied by turbine efficiency
+        self.pump_work= self.state4.h - self.state3.h # calculate pump work
+        self.heat_added= self.state1.h - self.state4.h # calculate heat added
+        self.efficiency=100.0*(self.turbine_work - self.pump_work)/self.heat_added
+        return self.efficiency
+
+    def print_summary(self):
+
+        if self.efficiency==None:
+            self.calc_efficiency()
+        print('Cycle Summary for: ', self.name)
+        print('\tEfficiency: {:0.3f}%'.format(self.efficiency))
+        print('\tTurbine Work: {:0.3f} kJ/kg'.format(self.turbine_work))
+        print('\tPump Work: {:0.3f} kJ/kg'.format(self.pump_work))
+        print('\tHeat Added: {:0.3f} kJ/kg'.format(self.heat_added))
+        self.state1.print()
+        self.state2.print()
+        self.state3.print()
+        self.state4.print()
+
+    def plot_cycle_TS(self):
+        """
+        This function will graph the rankine cycle from HW6 part 3 on a T-S diagram.
+        The two halves of the main curve,SaturatedLiquidLine and SaturatedVaporLine, are from sat_water_table.txt file.
+        Graph also includes isobars for plow and phigh (constructed from state objects above and steam objects in Steam_work.py).
+        :return: none, just the graph
+        """
+        ts, ps, hfs, hgs, sfs, sgs, vfs, vgs = np.loadtxt('sat_water_table.txt', skiprows=1, unpack=True) #reads values from water table txt file
+        #tcol, hcol, scol, pcol = np.loadtxt('superheated_water_table.txt', skiprows=1, unpack=True)
+        plt.xlim(0,8.99) #sets limits on x
+        plt.ylim(0,550) #sets limits on y
+        plt.plot(sfs,ts) #plots the sat fluid entropy vs. Tsat
+        plt.plot(sgs,ts,color='red') #plots the sat vapor entropy vs. Tsat #resposible for the right half of the curve
+        x1vals=[self.state3.s,self.state2.s]
+        y1vals=[self.state3.T,self.state2.T]
+        plt.plot(x1vals,y1vals, color='black') #plots the plow isobar
+        xs=steam(8000,x=0) #these 4 lines are the 2 points near the vapor dome
+        p1,p2=xs.s,xs.T
+        xt=steam(8000,x=1)
+        p3,p4=xt.s,xt.T
+        x2vals=[self.state3.s,p1,p3,self.state1.s,self.state2.s] #x vals for green curve
+        y2vals=[self.state3.T,p2,p4,self.state1.T,self.state2.T] #y vals for green curve
+        #print(self.state1.s)
+        plt.plot(x2vals,y2vals, color='green') #plots the phigh isobar
+        plt.fill_between(x2vals,y2vals,self.state2.T,facecolor='gray',alpha=0.15) #fills in the graph between phigh and plow isobars
+        plt.xlabel(r'S $\left(\frac{kJ}{{kg\cdot K}}\right)$', fontsize=12) #xlabel
+        plt.ylabel(r'T $\left(^o C\right)$', fontsize=12) #ylabel
+        plt.text(0.5,380,'Summary:\n$\eta$: {:0.1f}%\n$\eta_{}:$ {:0.2f}\n$W_{}$: {:0.1f} kJ/kg\n$W_{}$: {:0.1f} kJ/kg\n$Q_{}$: {:0.1f} kJ/kg'
+                 .format(self.efficiency,'{turbine}',self.eff_turbine,"{turbine}",self.turbine_work,"{pump}",self.pump_work,"{boiler}",self.heat_added))
+        #this Summary was so fun to figure out how to write....turns out you need those brackets around the string in the format section...
+        #unless you only want the first letter to be subscripted...*pterodactyl screech*
+        plt.plot(self.state1.s,self.state1.T,marker='o', markerfacecolor='white', markeredgecolor='black', markersize=6) #state markers
+        plt.plot(self.state2.s,self.state2.T,marker='o', markerfacecolor='white', markeredgecolor='black', markersize=6)
+        plt.plot(self.state3.s,self.state3.T,marker='o', markerfacecolor='white', markeredgecolor='black', markersize=6)
+        plt.title(self.name)
+        plt.show()
+        pass
+
+def main(): # This doesn't run for Q3 of the exam so I initially fed it values to check the numbers for my summary
+    rankine1= rankine(8,8000,t_high=500,eff_turbine=0.95,name='Rankine Cycle - Superheated at turbine inlet') #instantiate a rankine object to test it.
+    #t_high is specified
+    #if t_high were not specified, then x_high = 1 is assumed
+    eff=rankine1.calc_efficiency()
+    print(eff)
+    rankine1.print_summary()
+    rankine1.plot_cycle_TS()
+
+if __name__=="__main__":
+    main()
---
+++ Steam.py
@@ -0,0 +1,143 @@
+import numpy as np
+from scipy.interpolate import griddata
+
+class steam():
+    """
+    The steam class is used to find thermodynamic properties of steam along an isobar.
+    The Gibbs phase rule tells us we need two independent properties in order to find
+    all the other thermodynamic properties.  Hence, the constructor requires press of
+    the isobar and one other property.
+    """
+    def __init__(self, pressure, T=None, x=None, v=None, h=None, s=None, name=None):
+        '''
+        constructor for steam
+        :param pressure: pressure in kPa
+        :param T: Temperature in degrees C
+        :param x: quality of steam x=1 is saturated vapor, x=0 is saturated liquid
+        :param v: specific volume in m^3/kg
+        :param h: specific enthalpy in kJ/kg
+        :param s: specific entropy in kJ/(kg*K)
+        :param name: a convenient identifier
+        '''
+        #assign arguments to class properties
+        self.p = pressure #pressure - kPa
+        self.T = T #Temperature - degrees C
+        self.x = x #quality
+        self.v = v #specific volume - m^3/kg
+        self.h = h #specific enthalpy - kj/kg
+        self.s = s #entropy - kj/(kg*K)
+        self.name = name #a useful identifier
+        self.region = None #'superheated' or 'saturated' or 'two-phase'
+        if T==None and x==None and v==None and h==None and s==None: return
+        else: self.calc()
+
+    def calc(self):
+        '''
+        The Rankine cycle operates between two isobars (i.e., p_high (Turbine inlet state 1) & p_low (Turbine exit state 2)
+        So, given a pressure, we need to determine if the other given property puts
+        us in the saturated or superheated region.
+        :return: nothing returned, just set the properties
+        '''
+        #1. need to determine which second property is known
+        #2. determine if two-phase/saturated or superheated
+        #3. find all unknown thermodynamic properties by interpolation from appropriate steam table
+
+        #read in the thermodynamic data from files
+        ts, ps, hfs, hgs, sfs, sgs, vfs, vgs= np.loadtxt('sat_water_table.txt',skiprows=1, unpack=True) #use np.loadtxt to read the saturated properties
+        tcol, hcol, scol, pcol = np.loadtxt('superheated_water_table.txt', skiprows=1, unpack=True) #use np.loadtxt to read the superheated properties
+
+        R=8.314/(18/1000) #ideal gas constant for water [J/(mol K)]/[kg/mol]
+        Pbar=self.p/100 #pressure in bar - 1bar=100kPa roughly
+
+        #get saturated properties
+        #note: these are local variables only.  Their scope is only in this function
+        Tsat = float(griddata((ps), ts, (Pbar)))
+        hf=float(griddata((ps),hfs,(Pbar)))
+        hg=float(griddata((ps),hgs,(Pbar)))
+        sf=float(griddata((ps),sfs,(Pbar)))
+        sg=float(griddata((ps),sgs,(Pbar)))
+        vf=float(griddata((ps),vfs,(Pbar)))
+        vg=float(griddata((ps),vgs,(Pbar)))
+
+        self.hf=hf #this creates a member variable for the class that can be accessed from an object
+
+        #region determine which second property is given
+        if self.T is not None:
+            if self.T>Tsat: #interpolate with griddata
+                self.region='Superheated'
+                self.h = float(griddata((tcol, pcol), hcol, (self.T, self.p)))  #use griddata to interpolate with T & P the superheated table
+                self.s = float(griddata((tcol, pcol), scol, (self.T, self.p)))  #use griddata to interpolate with T & P the superheated table
+                self.x=1.0
+                TK = self.T + 273.14  # temperature conversion to Kelvin
+                self.v=R*TK/(self.p*1000)  #ideal gas approximation for volume
+        elif self.x!=None: #manual interpolation
+            self.region='Saturated'
+            self.T=Tsat
+            self.h=hf+self.x*(hg-hf)
+            self.s=sf+self.x*(sg-sf)
+            self.v=vf+self.x*(vg-vf)
+        elif self.h!=None:
+            self.x=(self.h-hf)/(hg-hf)
+            if self.x<=1.0: #manual interpolation
+                self.region='Saturated'
+                self.T=Tsat
+                self.s=sf+self.x*(sg-sf)
+                self.v=vf+self.x*(vg-vf)
+            else: #interpolate with griddata
+                self.region='Superheated'
+                self.T = float(griddata((hcol, pcol), tcol, (self.h, self.p)))  #use griddata to interpolate with h & P the superheated table
+                self.s = float(griddata((hcol, pcol), scol, (self.h, self.p)))  #use griddata to interpolate with h & P the superheated table
+        elif self.s!=None:
+            self.x=(self.s-sf)/(sg-sf)
+            if self.x<=1.0: #manual interpolation
+                self.region='Saturated'
+                self.T=Tsat
+                self.h=hf+self.x*(hg-hf)
+                self.v=vf+self.x*(vg-vf)
+            else: #interpolate with griddata
+                self.region = 'Superheated'
+                self.T = float(griddata((scol, pcol), tcol, (self.s, self.p)))  #use griddata to interpolate with s & P the superheated table
+                self.h = float(griddata((scol, pcol), scol, (self.s, self.p)))  #use griddata to interpolate with s & P the superheated table
+        #endregion
+
+    def print(self):
+        """
+        This prints a nicely formatted report of the steam properties.
+        :return: nothing, just prints to screen
+        """
+        print('Name: ', self.name)
+        if self.x<0.0: print('Region: compressed liquid')
+        else: print('Region: ', self.region)
+        print('p = {:0.2f} kPa'.format(self.p))
+        if self.x >= 0.0: print('T = {:0.1f} degrees C'.format(self.T))
+        print('h = {:0.2f} kJ/kg'.format(self.h))
+        if self.x >= 0.0:
+            print('s = {:0.4f} kJ/(kg K)'.format(self.s))
+            if self.region == 'Saturated': print('v = {:0.6f} m^3/kg'.format(self.v))
+            if self.region == 'Saturated': print('x = {:0.4f}'.format(self.x))
+        print()
+
+def main():
+    inlet=steam(7350,name='Turbine Inlet') #not enough information to calculate
+    inlet.x=0.9 #90 percent quality
+    inlet.calc()
+    inlet.print()
+
+    h1=inlet.h
+    s1=inlet.s
+    print(h1,s1,'\n')
+
+    outlet=steam(100, s=inlet.s, name='Turbine Exit')
+    outlet.print()
+
+    another=steam(8575, h=2050, name='State 3')
+    another.print()
+
+    yetanother = steam(8575, h=3125, name='State 4')
+    yetanother.print()
+
+#the following if statement causes main() to run
+#only if this file is being run explicitly, not if it is
+#being imported into another Python program as a module
+if __name__=="__main__":
+    main()
