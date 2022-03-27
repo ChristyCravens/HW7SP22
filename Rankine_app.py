6a03411b07767dabf778a2ab6c0e68520815f347
Christy C <102319832+ChristyCravens@users.noreply.github.com>
2022-03-26 22:58:21 -0500

    Update Rankine_app.py

--- Rankine_app.py
+++ Rankine_app.py
@@ -63,8 +63,8 @@ class main_window(QWidget, Ui_Form):
         self.le_TurbineWork.setText(str(round(self.rankine.turbine_work, 2)))
         
         # print the text at the bottom of the input section for the calculated values of PSat, hf, sf, and vf according to high or low pressure
-        self.lbl_SatPropHigh.setText("High Pressure Saturated Properties \nPSat = {:.2f} bar, TSat= {:.2f} c\nhf = {:.2f} kJ/kg , hg = {:.2f} kJ/kg\nsf= {:.2f} kJ/kg*K, sg= {:.2f} kJ/kg*K\nvf= {:.4f} m^3/kg, vg= {:.4f} m^3/kg" .format(self.rankine.p_high/100, self.rankine.state1.T, self.rankine.state1.hf, self.rankine.state1.hg, self.rankine.state1.sf, self.rankine.state1.sg, self.rankine.state1.vf,self.rankine.state1.vg))
-        self.lbl_SatPropLow.setText("Low Pressure Saturated Properties\nPSat = {:.2f} bar, TSat= {:.2f} c\nhf = {:.2f} kJ/kg , hg = {:.2f} kJ/kg\nsf= {:.2f} kJ/kg*K, sg= {:.2f} kJ/kg*K\nvf= {:.4f} m^3/kg, vg= {:.4f} m^3/kg" .format(self.rankine.p_low/100, self.rankine.state2.T, self.rankine.state2.hf, self.rankine.state2.hg, self.rankine.state2.sf, self.rankine.state2.sg, self.rankine.state2.vf,self.rankine.state2.vg))
+        self.lbl_SatPropHigh.setText("High Pressure Saturated Properties \nPSat = {:.2f} bar, TSat= {:.2f} c\nhf = {:.2f} kJ/kg , hg = {:.2f} kJ/kg\nsf= {:.2f} kJ/kg*K, sg= {:.2f} kJ/kg*K\nvf= {:.4f} m^3/kg, vg= {:.2f} m^3/kg" .format(self.rankine.p_high/100, self.rankine.state1.T, self.rankine.state1.hf, self.rankine.state1.hg, self.rankine.state1.sf, self.rankine.state1.sg, self.rankine.state1.vf,self.rankine.state1.vg))
+        self.lbl_SatPropLow.setText("Low Pressure Saturated Properties\nPSat = {:.2f} bar, TSat= {:.2f} c\nhf = {:.2f} kJ/kg , hg = {:.2f} kJ/kg\nsf= {:.2f} kJ/kg*K, sg= {:.2f} kJ/kg*K\nvf= {:.4f} m^3/kg, vg= {:.2f} m^3/kg" .format(self.rankine.p_low/100, self.rankine.state2.T, self.rankine.state2.hf, self.rankine.state2.hg, self.rankine.state2.sf, self.rankine.state2.sg, self.rankine.state2.vf,self.rankine.state2.vg))
 
         return
 
