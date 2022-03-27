c05effc2ad53dff79d01439eacb28a8b9320cd57
Christy C <102319832+ChristyCravens@users.noreply.github.com>
2022-03-26 22:57:55 -0500

    Update Rankine_app.py

--- Rankine_app.py
+++ Rankine_app.py
@@ -5,6 +5,7 @@ from Calc_state import Steam_SI as steam
 from Rankine import rankine
 from Steam import steam
 from PyQt5 import QtCore, QtGui, QtWidgets
+
 class main_window(QWidget, Ui_Form):
     def __init__(self):
         """
@@ -15,7 +16,7 @@ class main_window(QWidget, Ui_Form):
         self.setWindowTitle('Rankine Cycle Calculator') # set the window title
 
         self.rankine = rankine()  # instantiate a rankine object
-        #self.Steam = steam()
+        # create labels for all necessary values for calculations
         self.label = [self.le_PHigh, self.le_PLow, self.le_TurbineInletCondition, self.le_TurbineEff, self.le_H1,
                         self.le_H2, self.le_H3, self.le_H4, self.le_HeatAdded, self.le_PumpWork, self.le_Efficiency,
                         self.le_TurbineWork]
@@ -41,23 +42,27 @@ class main_window(QWidget, Ui_Form):
         #otherwise, leave it as is to fill in after calculated
         self.rankine.quality = float(self.le_TurbineInletCondition.text()) if self.rdo_Quality.isChecked() else None
         self.rankine.t_high = float(self.le_TurbineInletCondition.text()) if self.rdo_THigh.isChecked() else None
+        # if Quality/THigh is checked, set the text accordingly
         if self.rdo_Quality.isChecked():
             self.lbl_TurbineInletCondition.setText(_translate("Form", "Turbine Inlet:x ="))
         if self.rdo_THigh.isChecked():
             self.lbl_TurbineInletCondition.setText(_translate("Form","Turbine Inlet:T_high ="))
 
         self.rankine.calc_efficiency()
-
+        
+        # fill text boxes with corresponding calculated values for h1, h2, h3, and h4
         self.le_H1.setText(str(round(self.rankine.state1.h, 2)))
         self.le_H2.setText(str(round(self.rankine.state2.h, 2)))
         self.le_H3.setText(str(round(self.rankine.state3.h, 2)))
         self.le_H4.setText(str(round(self.rankine.state4.h, 2)))
 
+        # fill text boxes with corresponding calculated values for heat added, pump work, thermal efficiency, and turbine work
         self.le_HeatAdded.setText(str(round(self.rankine.heat_added, 2)))
         self.le_PumpWork.setText(str(round(self.rankine.pump_work, 2)))
         self.le_Efficiency.setText(str(round(self.rankine.efficiency, 2)))
         self.le_TurbineWork.setText(str(round(self.rankine.turbine_work, 2)))
-
+        
+        # print the text at the bottom of the input section for the calculated values of PSat, hf, sf, and vf according to high or low pressure
         self.lbl_SatPropHigh.setText("High Pressure Saturated Properties \nPSat = {:.2f} bar, TSat= {:.2f} c\nhf = {:.2f} kJ/kg , hg = {:.2f} kJ/kg\nsf= {:.2f} kJ/kg*K, sg= {:.2f} kJ/kg*K\nvf= {:.4f} m^3/kg, vg= {:.4f} m^3/kg" .format(self.rankine.p_high/100, self.rankine.state1.T, self.rankine.state1.hf, self.rankine.state1.hg, self.rankine.state1.sf, self.rankine.state1.sg, self.rankine.state1.vf,self.rankine.state1.vg))
         self.lbl_SatPropLow.setText("Low Pressure Saturated Properties\nPSat = {:.2f} bar, TSat= {:.2f} c\nhf = {:.2f} kJ/kg , hg = {:.2f} kJ/kg\nsf= {:.2f} kJ/kg*K, sg= {:.2f} kJ/kg*K\nvf= {:.4f} m^3/kg, vg= {:.4f} m^3/kg" .format(self.rankine.p_low/100, self.rankine.state2.T, self.rankine.state2.hf, self.rankine.state2.hg, self.rankine.state2.sf, self.rankine.state2.sg, self.rankine.state2.vf,self.rankine.state2.vg))
 
